3b604836e2580b0879a1c841f1dcbdf610ab7c64
Craxy <Craxyss@gmail.com>
2016-08-06 13:46:48 +0200

    implemented: mod behaviour

---
+++ src/FearlessGuests/Mod.cs
@@ -0,0 +1,30 @@
+﻿namespace Craxy.Parkitect.FearlessGuests
+{
+    public class Mod : IMod
+    {
+        public string Name
+        {
+            get { return "Fearless Guests"; }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return "Guests ignore the intensity ratings of rides. As a result all guests like all rides the same.";
+            }
+        }
+
+        public string Identifier { get; set; }
+
+        public void onEnabled()
+        {
+            Global.GUESTS_LIKE_EVERY_RIDE = true;
+        }
+
+        public void onDisabled()
+        {
+            Global.GUESTS_LIKE_EVERY_RIDE = false;
+        }
+    }
+}
